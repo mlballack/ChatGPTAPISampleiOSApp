d152b205b818ab6ba3db423674f0a5794220f6e9
m-hayashi13 <m-hayashi@advanced-media.co.jp>
2023-03-02 10:47:25 +0900

    Initial Commit

---
+++ ChatGPTAPISample/ChatGPTAPISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAPISampleApp.swift
+//  ChatGPTAPISample
+//
+//  Created by 林 政樹 on 2023/03/02.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAPISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
